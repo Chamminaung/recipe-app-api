068278ab53997c2702b5d918bf39114d598456eb
Chamminaung <chamminaung25@gmail.com>
2025-09-09 13:21:45 +0630

    initial Commit

---
+++ src/config/env.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const ENV = {
+    PORT: process.env.PORT || 5000,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret',
+    // Add other environment variables as needed
+}
